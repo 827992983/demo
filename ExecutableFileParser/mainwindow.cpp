dee3356ca87c4afeb328ac70c527224689f6b7cc
827992983 <827992983@qq.com>
2020-12-17 15:29:45 +0800

    update ExecutableFileParser

--- ExecutableFileParser/mainwindow.cpp
+++ ExecutableFileParser/mainwindow.cpp
@@ -120,13 +120,67 @@ void MainWindow::on_actionParseDosHeader_triggered()
         QMessageBox::information(this, APP_NAME, "这不是PE格式文件！");
         return;
     }
-    appendTextEdit("------------------------------DOS头------------------------------");
+    appendTextEdit("------------------------------DOS头[十六进制]------------------------------");
     char buf[1024] = {0};
-    sprintf(buf, " e_magic:%8x                //MZ，DOS头的幻数", pDosHeader->e_magic);
+    sprintf(buf, "     e_magic:%04x                //MZ，DOS头的幻数", pDosHeader->e_magic);
     appendTextEdit(QString(buf));
     memset(buf, 0, 1024);
-    sprintf(buf, "e_lfanew:%8x                //PE头相对于文件的偏移，用于定位PE文件（具体值会由于编译器不同，具体值不一定）", pDosHeader->e_lfanew);
+    sprintf(buf, "      e_cblp:%04x                //[Bytes on last page of file", pDosHeader->e_cblp);
     appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "        e_cp:%04x                //Pages in file", pDosHeader->e_cp);
+    appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "      e_crlc:%04x                //Relocations", pDosHeader->e_crlc);
+    appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "   e_cparhdr:%04x                //Size of header in paragraphs", pDosHeader->e_cparhdr);
+    appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "  e_minalloc:%04x                //Minimum extra paragraphs needed", pDosHeader->e_minalloc);
+    appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "  e_maxalloc:%04x                //Maximum extra paragraphs needed", pDosHeader->e_maxalloc);
+    appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "        e_ss:%04x                //DOS代码的初始化堆栈SS值", pDosHeader->e_ss);
+    appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "        e_sp:%04x                //DOS代码的初始化堆栈指针SP值", pDosHeader->e_sp);
+    appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "      e_csum:%04x                //Checksum", pDosHeader->e_csum);
+    appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "        e_ip:%04x                //DOS代码入口IP", pDosHeader->e_ip);
+    appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "        e_cs:%04x                //DOS代码的入口CS", pDosHeader->e_cs);
+    appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "    e_lfarlc:%04x                //File address of relocation table", pDosHeader->e_lfarlc);
+    appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "      e_ovno:%04x                //Overlay number", pDosHeader->e_ovno);
+    appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "       e_res:%04x %04x %04x %04x //Reserved words", pDosHeader->e_res[0],pDosHeader->e_res[1],pDosHeader->e_res[2],pDosHeader->e_res[3]);
+    appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "     e_oemid:%04x                //OEM identifier (for e_oeminfo)", pDosHeader->e_oemid);
+    appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "   e_oeminfo:%04x                //OEM information; e_oemid specific", pDosHeader->e_oeminfo);
+    appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "      e_res2:%04x %04x %04x %04x %04x %04x %04x %04x %04x %04x //Reserved words.", pDosHeader->e_res2[0],
+            pDosHeader->e_res2[1],pDosHeader->e_res2[2],pDosHeader->e_res2[3],pDosHeader->e_res2[4],pDosHeader->e_res2[5],
+            pDosHeader->e_res2[6],pDosHeader->e_res2[7],pDosHeader->e_res2[8],pDosHeader->e_res2[9]);
+    appendTextEdit(QString(buf));
+    memset(buf, 0, 1024);
+    sprintf(buf, "    e_lfanew:%08x            //PE头相对于文件的偏移，用于定位PE文件（具体值会由于编译器不同，具体值不一定）", pDosHeader->e_lfanew);
+    appendTextEdit(QString(buf));
+
 }
 
 void MainWindow::on_actionParsePeHeader_triggered()
